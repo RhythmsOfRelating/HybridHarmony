6b83e963c376878f22c3c5c557d2df3cccb5f42e
phoebe <hchen03@wesleyan.edu>
2020-07-09 01:09:21 -0400

    added different metrics

--- LSLanalysis/analysis/correlation_perFreq.py
+++ LSLanalysis/analysis/correlation_perFreq.py
@@ -9,6 +9,8 @@ from itertools import islice
 from pylsl import local_clock, StreamInfo, StreamOutlet, IRREGULAR_RATE, cf_float32
 from scipy.signal import hilbert
 from scipy.signal import butter, lfilter
+from astropy.stats import circmean
+from itertools import product
 
 current = os.path.dirname(__file__)
 PATH_TOPO = os.path.join(current, 'topo.json')
@@ -109,7 +111,6 @@ class Correlation:
         info.desc().append_child_value('subjects', '_'.join(list(self.buffers.keys())))
         return StreamOutlet(info)
 
-
     def _setup_coefficients(self):
         nyq = 0.5 * self.sample_rate
         min_band = 0.0 + 1.0e-10
@@ -142,16 +143,15 @@ class Correlation:
             LAST_CALCULATION = trailing_timestamp
             analysis_window = self._select_analysis_window(trailing_timestamp)
             self._apply_window_weights(analysis_window)
-            # phoebe edit
-            envelope_matrix = self._calculate_all(analysis_window)
-            rvalues = self._calculate_rvalues(envelope_matrix)
-            power_values = self._calculate_power(envelope_matrix)
+
+            analytic_matrix = self._calculate_all(analysis_window)
+            rvalues = self._calculate_rvalues(analytic_matrix)
+            power_values = self._calculate_power(analytic_matrix)
             
             if OUTLET:
                 self.logger.debug("Sending {} R values with timestamp {}".format(len(rvalues), trailing_timestamp))
                 OUTLET.push_sample(rvalues, timestamp=trailing_timestamp)
             if OUTLET_POWER:
-                # phoebe edit
                 OUTLET_POWER.push_sample(power_values, timestamp=trailing_timestamp)
 
         else:
@@ -163,49 +163,8 @@ class Correlation:
             analysis_window[uid] = np.multiply(analysis_window[uid], HANN[:, None])
         self.logger.debug("Applying window weights with %s samples and % channels." % analysis_window[uid].shape)
 
-    # phoebe edit: using envelop matrix to calculate both rvals and powers (so to avoid calculate hilbert transform twice)
-    def _calculate_power(self, envelope_matrix):
-        return np.mean(envelope_matrix, axis=3).reshape(-1)
-    
-    def _calculate_rvalues(self, envelope_matrix):
-
-        rvalues = []
-        
-        for window_idx in range(len(envelope_matrix) - 1):
-            compare_windows = len(envelope_matrix) - window_idx - 1
-            compare_window_idx = 1
-            while compare_window_idx <= compare_windows:
-                # for each frequency, compute corr for all electrodes, and average over selected ones
-                for freq, freq_name in enumerate(list(BANDS.keys())):
-                    rval_freq = []
-                    for electrode in range(CHANNEL_COUNT):
-                        r = np.corrcoef([envelope_matrix[window_idx, electrode, freq, :],
-                                         envelope_matrix[compare_window_idx, electrode, freq, :]])[0][1]
-                        rval_freq.append(r)
-                    rval_freq = np.array(rval_freq)
-                    rvalues.append(np.mean(rval_freq[CHOOSE_CHANNELS[freq_name]]))
-                compare_window_idx += 1
-
-        return rvalues  # n_connections x n_freq
-
-    def _calculate_envelope(self, signal, coeff):  # phoebe edit
-        
-        signal = lfilter(coeff[0], coeff[1], signal)
-        
-        analytic_signal = hilbert(signal)
-        amplitude_envelope = np.abs(analytic_signal)
-        
-        return amplitude_envelope
-    
-    # phoebe edit: calculating for all subjects, the envelope matrix (n_subject x n_channels x n_freq x n_samples)
-    def _calculate_all(self, analysis_window):
-        all_envelope = np.empty((len(analysis_window), int(self.channel_count), len(COEFFICIENTS), int(self.sample_rate * WINDOW)))  # n_persons x n_channels x n_freq x n_samples
-        for i, current_window in enumerate(analysis_window.keys()):
-            for channel in range(self.channel_count):
-                for c, coeff in enumerate(COEFFICIENTS):
-                    envelope = self._calculate_envelope(analysis_window[current_window][:,channel], coeff)
-                    all_envelope[i, channel, c, :] = envelope
-        return all_envelope
+    def _calculate_power(self, analytic_matrix):
+        return np.nanmean(np.abs(analytic_matrix)**2, axis=3).reshape(-1)
     
     # read json files and choose electrodes for each freq band, saved as a dict
     def _setup_electrodes(self):
@@ -235,7 +194,6 @@ class Correlation:
         return choose
 
 
-
     def _find_trailing_timestamp(self):
         trailing_timestamp = local_clock()
         
@@ -262,3 +220,110 @@ class Correlation:
                 analysis_window[uid] = np.array([sample[1] for sample in timestamped_window])
 
         return analysis_window
+
+    def _calculate_analytic(self, signal, coeff):
+        signal = lfilter(coeff[0], coeff[1], signal)
+        analytic_signal = hilbert(signal)
+        return analytic_signal
+
+    def _calculate_all(self, analysis_window):
+        all_analytic = np.array(list(analysis_window.values())).reshape((len(analysis_window), self.channel_count, -1))
+
+        all_analytic = np.array([self._calculate_analytic(all_analytic, coeff) for c, coeff in enumerate(COEFFICIENTS)])
+
+        return all_analytic
+
+    # helper function
+    def _multiply_conjugate(self, real: np.ndarray, imag: np.ndarray, transpose_axes: tuple) -> np.ndarray:
+        """
+        Helper function to compute the product of a complex array and its conjugate.
+        It is designed specifically to collapse the last dimension of a four-dimensional array.
+        Arguments:
+            real: the real part of the array.
+            imag: the imaginary part of the array.
+            transpose_axes: axes to transpose for matrix multiplication.
+        Returns:
+            product: the product of the array and its complex conjugate.
+        """
+        formula = 'ilm,imk->ilk'
+        product = np.einsum(formula, real, real.transpose(transpose_axes)) + \
+                  np.einsum(formula, imag, imag.transpose(transpose_axes)) - 1j * \
+                  (np.einsum(formula, real, imag.transpose(transpose_axes)) - \
+                   np.einsum(formula, imag, real.transpose(transpose_axes)))
+
+        return product
+
+    def compute_sync(self, complex_signal: np.ndarray, mode: str) -> np.ndarray:
+
+        n_ch, n_freq, n_samp = complex_signal.shape[2], complex_signal.shape[0], \
+                               complex_signal.shape[3]
+
+        # calculate all epochs at once, the only downside is that the disk may not have enough space
+        complex_signal = complex_signal.reshape(n_freq, 2 * n_ch, n_samp)
+        transpose_axes = (0, 2, 1)
+        if mode.lower() == 'plv':
+            phase = complex_signal / np.abs(complex_signal)
+            c = np.real(phase)
+            s = np.imag(phase)
+            dphi = self._multiply_conjugate(c, s, transpose_axes=transpose_axes)
+            con = abs(dphi) / n_samp
+
+        elif mode.lower() == 'envelope_corr':
+            env = np.abs(complex_signal)
+            mu_env = np.mean(env, axis=2).reshape(n_freq, 2 * n_ch, 1)
+            env = env - mu_env
+            con = np.einsum('ilm,imk->ilk', env, env.transpose(transpose_axes)) / \
+                  np.sqrt(np.einsum('il,ik->ilk', np.sum(env ** 2, axis=2), np.sum(env ** 2, axis=2)))
+
+        elif mode.lower() == 'pow_corr':
+            env = np.abs(complex_signal) ** 2
+            mu_env = np.mean(env, axis=3).reshape(n_freq, 2 * n_ch, 1)
+            env = env - mu_env
+            con = np.einsum('ilm,imk->ilk', env, env.transpose(transpose_axes)) / \
+                  np.sqrt(np.einsum('il,ik->ilk', np.sum(env ** 2, axis=2), np.sum(env ** 2, axis=2)))
+
+        elif mode.lower() == 'coh':
+            c = np.real(complex_signal)
+            s = np.imag(complex_signal)
+            amp = np.abs(complex_signal) ** 2
+            dphi = self._multiply_conjugate(c, s, transpose_axes=transpose_axes)
+            con = np.abs(dphi) / np.sqrt(np.einsum('nil,nik->nilk', np.nansum(amp, axis=3),
+                                                   np.nansum(amp, axis=3)))
+
+        elif mode.lower() == 'imaginary_coh':
+            c = np.real(complex_signal)
+            s = np.imag(complex_signal)
+            amp = np.abs(complex_signal) ** 2
+            dphi = self._multiply_conjugate(c, s, transpose_axes=transpose_axes)
+            con = np.abs(np.imag(dphi)) / np.sqrt(np.einsum('il,ik->ilk', np.nansum(amp, axis=2),
+                                                            np.nansum(amp, axis=2)))
+
+        elif mode.lower() == 'ccorr':
+            angle = np.angle(complex_signal)
+            mu_angle = circmean(angle, axis=3).reshape(n_freq, 2 * n_ch, 1)
+            angle = np.sin(angle - mu_angle)
+
+            formula = 'ilm,imk->ilk'
+            con = np.einsum(formula, angle, angle.transpose(transpose_axes)) / \
+                  np.sqrt(np.einsum('il,ik->ilk', np.sum(angle ** 2, axis=2), np.sum(angle ** 2, axis=2)))
+
+        else:
+            ValueError('Metric type not supported.')
+
+        return con
+
+    def _calculate_rvalues(self, analytic_matrix, mode):
+        pair_index = [a for a in
+                      list(product(np.arange(0, analytic_matrix.shape[1]), np.arange(0, analytic_matrix.shape[1])))
+                      if a[0] < a[1]]
+        rvals = []
+        for pair in pair_index:
+            con = self.compute_sync(analytic_matrix[:, pair, :, :], mode)
+            # the connectivity matrix for the current pair. shape is (n_freq, n_ch, n_ch)
+            con = con[:, 0:self.channel_count, self.channel_count:]
+            # TODO: average certain channels based on frequency bands. Question: how do we organize channel info?
+            rvals.append(np.nanmean(con, axis=(1, 2)))
+
+        return np.array(rvals)  # (n_connections, n_freq)
+
+
