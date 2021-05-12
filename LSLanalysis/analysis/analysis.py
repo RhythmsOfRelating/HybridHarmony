8cbb9fc5ff591d3a8dbe4a1aab78676e5844f9e8
phoebsc <34769000+phoebsc@users.noreply.github.com>
2021-05-12 10:23:51 -0400

    Update analysis.py
    manual update

--- LSLanalysis/analysis/analysis.py
+++ LSLanalysis/analysis/analysis.py
@@ -1,8 +1,19 @@
 import logging
 from threading import current_thread, Thread
 from collections import deque
+from os import getpid
 from .buffer import Buffer
 from .correlation_perFreq import Correlation
+from multiprocessing import Queue
+from pylsl import local_clock, StreamInfo, StreamOutlet, IRREGULAR_RATE, cf_float32
+from scipy.signal import butter
+from itertools import product
+from osc4py3.as_allthreads import *
+from osc4py3 import oscbuildparse
+from osc4py3 import oscchannel as osch
+import numpy as np
+ORDER = 5
+WINDOW = 3
 
 class Analysis:
   def __init__(self, discovery, mode, chn_type, corr_params, OSC_params, compute_pow, window_params, norm_params):
@@ -11,6 +22,7 @@ class Analysis:
     self.mode = mode
     self.chn_type = chn_type
     self.corr_params = corr_params
+    self.freqParams = corr_params[0]
     self.buffer = Buffer(discovery)
     self.OSC_params = OSC_params
     self.compute_pow = compute_pow
@@ -19,33 +31,35 @@ class Analysis:
     self.thread = None
     self.running = False
     self.corr = None
+    self.que = Queue(maxsize=1000)
+
+    self.buffer.pull()  # initial pull in order to setup connections
+    self.sample_rate = self.discovery.sample_rate
+    self.channel_count = self.discovery.channel_count
+    self.sample_size = int(self.sample_rate * WINDOW)
+    self.STREAM_COUNT = len(self.buffer.buffers_by_uid)
+    self.COEFFICIENTS = self._setup_coefficients()
+    self.HANN = self._setup_hann()
+    self.CONNECTIONS = self._setup_num_connections()
+    self.OUTLET = self._setup_outlet()
+    if self.compute_pow:
+      self.OUTLET_POWER = self._setup_outlet_power()
+    else:
+      self.OUTLET_POWER = None
 
   def start(self):
     if self.thread:
       return False
-
-    self.thread = Thread(target=self._update, daemon=True, name="Buffering")
+    self.thread = Thread(
+      target=self._update,
+      name="analysis",
+      args=[self.que],
+    )
+    # self.thread = Thread(target=lambda q, args1: q.put(self._update()),
+    #                      args=(self.que, None), daemon=True, name="Buffering")
+    # self.thread = Thread(target=self._update, daemon=True, name="Buffering")
     self.running = True
     self.thread.start()
-
-    # create correlation object
-    sample_rate = self.discovery.sample_rate
-    channel_count = self.discovery.channel_count
-    # Make sure we're still connected and have a sample rate
-    if not sample_rate or not channel_count:
-      self.logger.warning('connection broken.')
-      return
-
-    self.corr = Correlation(
-      sample_rate=sample_rate,
-      channel_count=channel_count,
-      buffers=self.buffer.buffers_by_uid,
-      mode=self.mode,
-      chn_type=self.chn_type,
-      corr_params=self.corr_params,
-      OSC_params=self.OSC_params,
-      compute_pow=self.compute_pow,
-      norm_params=self.norm_params)
 
     return True
 
@@ -60,17 +74,99 @@ class Analysis:
 
     return True
 
-  def _update(self):
+  def _update(self, que):
     while self.running:
       try:
         self.buffer.pull()
-        self._calculate()
+        r = self._calculate()
+        if r:
+          que.put(r)
       except Exception as e:
-        self.logger.warning("Error during analysis, skipped frame")
+        self.logger.warning("Error during analysis, skipped frame "+str(e))
         self.logger.exception(e)
+
 
   def _calculate(self):
     # Make sure we have buffers to analyze
     if len(self.buffer.buffers_by_uid) == 0:
       return
-    self.corr.run()
+    # create correlation object
+    self.sample_rate = self.discovery.sample_rate
+    self.channel_count = self.discovery.channel_count
+    # Make sure we're still connected and have a sample rate
+    if not self.sample_rate or not self.channel_count:
+      self.logger.warning('connection broken.')
+      return
+    self.corr = Correlation(
+      sample_rate=self.sample_rate,
+      channel_count=self.channel_count,
+      buffers=self.buffer.buffers_by_uid,
+      mode=self.mode,
+      chn_type=self.chn_type,
+      corr_params=self.corr_params,
+      OSC_params=self.OSC_params,
+      compute_pow=self.compute_pow,
+      norm_params=self.norm_params,
+      COEFFICIENTS=self.COEFFICIENTS,
+      HANN=self.HANN,
+      CONNECTIONS=self.CONNECTIONS,
+      OUTLET=self.OUTLET, OUTLET_POWER=self.OUTLET_POWER)
+    r = self.corr.run()
+    return r
+
+  # helper functions to set up
+
+  def _setup_outlet(self):
+    sample_size = self.CONNECTIONS * len(self.freqParams)
+    if sample_size == 0:
+      return
+
+    info = StreamInfo('RValues', 'Markers', sample_size, IRREGULAR_RATE, cf_float32, "RValues-{}".format(getpid()))
+    info.desc().append_child_value("correlation", "R")
+
+    mappings = info.desc().append_child("mappings")
+    buffer_keys = list(self.buffer.buffers_by_uid.keys())
+    pair_index = [a for a in
+                  list(product(np.arange(0, len(buffer_keys)), np.arange(0, len(buffer_keys))))
+                  if a[0] < a[1]]
+
+    for pair in pair_index:
+      mappings.append_child("mapping") \
+        .append_child_value("from", buffer_keys[pair[0]]) \
+        .append_child_value("to", buffer_keys[pair[1]])
+
+    return StreamOutlet(info)
+
+  # phoebe edit
+  def _setup_outlet_power(self):
+    sample_size = self.STREAM_COUNT * self.channel_count * len(self.freqParams)
+    self.logger.warning('power sample size is %s %s %s' % (self.STREAM_COUNT, self.channel_count, len(self.freqParams)))
+    if sample_size == 0:
+      return
+    info = StreamInfo('Powervals', 'Markers', sample_size, IRREGULAR_RATE, cf_float32, "Pvals-{}".format(getpid()))
+    info.desc().append_child_value('subjects', '_'.join(list(self.buffer.buffers_by_uid.keys())))
+    return StreamOutlet(info)
+
+  def _setup_coefficients(self):
+    nyq = 0.5 * self.sample_rate
+    min_band = 0.0 + 1.0e-10
+    max_band = 1.0 - 1.0e-10
+    coefficients = []
+    for band in self.freqParams.values():
+      low_band = max(min_band, min(max_band, band[0] / nyq))
+      high_band = max(min_band, min(max_band, band[1] / nyq))
+      low, high = butter(ORDER, [low_band, high_band], btype='band')
+      coefficients.append([low, high])
+
+    return coefficients
+
+  def _setup_hann(self):
+    denom = self.sample_size - 1
+    hann = []
+    for n in range(self.sample_size):
+      hann.append(.5 * (1 - np.cos((2 * np.pi * n) / denom)))
+
+    return np.array(hann)
+
+  def _setup_num_connections(self):
+    return sum(list(range(1, len(self.buffer.buffers_by_uid))))
