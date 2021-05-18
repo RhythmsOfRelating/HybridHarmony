35554400ec12b27c54fac52549c37bbf69c47e5e
phoebsc <34769000+phoebsc@users.noreply.github.com>
2021-05-18 09:38:12 -0400

    Update buffer.py

--- LSLanalysis/analysis/buffer.py
+++ LSLanalysis/analysis/buffer.py
@@ -1,13 +1,19 @@
+"""
+Buffer module is used in Analysis module to pull data from the Discovery object
+"""
 import logging
 import time
-
 from collections import deque
 from pylsl import local_clock
 
-BUFFER_WINDOW = 20 # seconds
+BUFFER_WINDOW = 20  # seconds
 
 class Buffer:
   def __init__(self, discovery):
+    """
+    Class pulling and cleaning data from Discovery object
+    :param discovery: Discovery object
+    """
     self.logger = logging.getLogger(__name__)
     self.discovery = discovery
     self.buffers_by_uid = {}
@@ -15,6 +21,9 @@ class Buffer:
     self.trailing_stream = None
 
   def pull(self):
+    """
+    Pull data chunks
+    """
     self._clean_buffers()
     self._process_buffers()
     self._update_trailing_stream()
@@ -27,9 +36,13 @@ class Buffer:
       time.sleep(0.1)
 
   def _update_trailing_stream(self):
+    """
+    update the latest timestamp and the last LSL stream
+    """
     trailing_timestamp = local_clock()
     trailing_stream = None
 
+    # make sure the current timestamp is no earlier than the latest timestamp of each stream
     for uid, buffer in self.buffers_by_uid.items():
       timestamp, _ = buffer[-1]
       if trailing_timestamp > timestamp:
@@ -40,18 +53,24 @@ class Buffer:
     self.trailing_stream = trailing_stream
 
   def _process_buffers(self):
+    """
+    retrieve samples from Discovery and save in a deque object
+    """
     for uid, stream in self.discovery.streams_by_uid.items():
       samples = stream.buffer.process(timeout=0.0)
 
       if len(samples) > 0:
         self.logger.debug("{}: Taking {} samples from buffer".format(stream.name, len(samples)))
-
+        # if uid is new, then create a new deque object
         if uid not in self.buffers_by_uid:
           self.buffers_by_uid[uid] = deque(samples, maxlen=int(stream.info.nominal_srate() * BUFFER_WINDOW))
-        else:
+        else:  # if uid already exists, simply save the samples
           self.buffers_by_uid[uid].extend(samples)
 
   def _clean_buffers(self):
+    """
+    remove streams if they are no longer available in Discovery
+    """
     for uid in set(self.buffers_by_uid.keys()) - set(self.discovery.streams_by_uid.keys()):
       self.logger.debug("Cleaning stored buffer for device with uid {}".format(uid))
       del self.buffers_by_uid[uid]
